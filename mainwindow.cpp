c4472c4d59ac2ddaf781d819e0d1410ec97b38df
Tamer Aktekin <aktekintamer@gmail.com>
2020-05-31 02:55:25 +0300

    Add buttons and axes. Include qwt.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,19 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // @TODO: Make below lines more beautiful.
+    ui->powerPlot->enableAxis(0, false);
+    ui->powerPlot->enableAxis(2, false);
+
+    ui->fftPlot->enableAxis(0, false);
+    ui->fftPlot->enableAxis(2, false);
+
+    ui->spectrogram->enableAxis(0, false);
+    ui->spectrogram->enableAxis(2, false);
+
+    ui->spectrogram3D->enableAxis(0, false);
+    ui->spectrogram3D->enableAxis(2, false);
 }
 
 MainWindow::~MainWindow()
--- spectamer.pro
+++ spectamer.pro
@@ -21,6 +21,9 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 CONFIG += c++11
+CONFIG += qwt
+
+include(C:/Qwt-6.1.4/features/qwt.prf) # Note: This line solved qwt problem.
 
 SOURCES += \
         main.cpp \
