813a2f2b67cae48bb850358ce830fce4d30299e6
tameraktekin <aktekintamer@gmail.com>
2020-12-22 22:14:35 +0300

    Remove unnecessary objects. Add time scale for spectrogram.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -60,9 +60,7 @@ void MainWindow::arrangePlots()
 
     ui->spectrogram->enableAxis(0, true);
     ui->spectrogram->enableAxis(2, true);
-
-    ui->spectrogram3D->enableAxis(0, true);
-    ui->spectrogram3D->enableAxis(2, true);
+    ui->spectrogram->setAxisScale(ui->spectrogram->xBottom, 0, (8 * fftLen) / sampleRate);
 }
 
 void MainWindow::on_optionsButton_clicked()
@@ -150,10 +148,9 @@ void MainWindow::updateFFTPlot(){
 
         if (stftCount >= (8 * fftLen)){
 
-            dataSpec->setInterval(Qt::XAxis, QwtInterval( 0, 16));
+            dataSpec->setInterval(Qt::XAxis, QwtInterval( 0, (8 * fftLen) / sampleRate));
             dataSpec->setInterval(Qt::YAxis, QwtInterval( 0, (sampleRate / 2) ));
             dataSpec->setInterval(Qt::ZAxis, QwtInterval( 0, 2 ));
-            qDebug() << dataSpec->numRows() << dataSpec->numColumns() << stftMag.length();
 
             spec->setData(dataSpec);
             spec->attach(ui->spectrogram);
